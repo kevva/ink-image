847dec837be9bbdaea7d0194e362d47c83755bfd
Vadim Demedes <vdemedes@gmail.com>
2019-03-04 00:21:07 -0800

    Use Ink 2.0.0

--- test.js
+++ test.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import {renderToString} from 'ink';
+import {render} from 'ink';
 import {string as termImgString} from 'term-img';
 import test from 'ava';
 import Image from '.';
+
+// Fake process.stdout
+class Stream {
+	constructor() {
+		this.output = '';
+		this.columns = 100;
+	}
+
+	write(str) {
+		this.output = str;
+	}
+
+	get() {
+		return this.output;
+	}
+}
+
+const renderToString = node => {
+	const stream = new Stream();
+
+	render(node, {
+		stdout: stream,
+		debug: true
+	});
+
+	return stream.get();
+};
 
 test('render', t => {
 	const actual = renderToString(<Image src='./fixture.png'/>);
