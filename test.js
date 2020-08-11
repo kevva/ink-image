461539457c786aefcb43701282db6c614343d205
Richie Bendall <richiebendall@gmail.com>
2020-08-12 03:28:15 +1200

    Switch from `term-img` to `terminal-image`
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -1,32 +1,27 @@
 import {serial as test} from 'ava';
 import React from 'react';
 import {render} from 'ink-testing-library';
-import {string as termImgString} from 'term-img';
+import terminalImage from 'terminal-image';
+import delay from 'delay';
 import Image from '.';
 
-test('render', t => {
+test('render', async t => {
 	const actual = render(<Image src='./fixture.png'/>);
-	const expected = termImgString('./fixture.png', {
-		fallback: () => ''
-	});
+	const expected = await terminalImage.file('./fixture.png');
+
+	await delay(2000);
 
 	t.is(actual.lastFrame(), expected);
 });
 
-test('pass props to ansi-escapes', t => {
-	const actual = render(<Image preserveAspectRatio src='./fixture.png' width='5px'/>);
-	const expected = termImgString('./fixture.png', {
-		width: '5px',
-		preserveAspectRatio: true,
-		fallback: () => ''
+test('pass props to ansi-escapes', async t => {
+	const actual = render(<Image preserveAspectRatio src='./fixture.png' width={5}/>);
+	const expected = await terminalImage.file('./fixture.png', {
+		width: 5,
+		preserveAspectRatio: true
 	});
 
-	t.is(actual.lastFrame(), expected);
-});
+	await delay(2000);
 
-test('alt text', t => {
-	const {TERM_PROGRAM} = process.env;
-	process.env.TERM_PROGRAM = 'Unicorn.app';
-	t.is(render(<Image src='./fixture.png' alt='Hello'/>).lastFrame(), 'Hello');
-	process.env.TERM_PROGRAM = TERM_PROGRAM;
+	t.is(actual.lastFrame(), expected);
 });
